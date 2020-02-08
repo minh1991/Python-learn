984f683357aad4534aaacf097904f2cdad17db2a
Minh <minh@Minhs-iMac.local>
2020-02-09 01:41:54 +0700

    20200208

--- A20200204.py
+++ A20200204.py
@@ -17,8 +17,8 @@ from decimal import *
 # tạo ra một phân số
 from fractions import *
 frac    =   Fraction(6,9)
-print(type(frac))
-print(frac)
+# print(type(frac))
+# print(frac)
 
 # số phức
 comp    =   complex(2,5)
@@ -26,5 +26,32 @@ print(type(comp))
 # lấy ra phần thực
 print(comp.real)
 # lấy ra phần ảo
-print(comp.imag)
-print(2**3)
+# print(comp.imag)
+# print(2**3)
+
+# 20200208
+# print("Obama",end=' ')
+# print("Xin chào",end=' ')
+# print("Putin")
+
+# print(end='Mời bạn nhập 1 số:')
+# a=input()
+# print('số bạn vừa nhập = ',a)
+
+# phép nhân chuỗi
+# strA    =   "hello boy \n"
+# strB    =   strA * 5
+# print(strB)
+
+
+strA    =   "helloBoyMaster"
+strB    =   strA[None:None:2]
+print(strB)
+# strC    =   strB in strA
+
+
+str1    = int("66")
+str2    = str1 + 10
+str3    = 60
+str4    = str(str3) + "10"
+print(str4,type(str3), type(str4))
--- test.py
+++
@@ -1,7 +0,0 @@
-msg="hello men"
-print(msg, type(msg))
-
-num_1 = 1.99999842389472364728367534534
-from decimal import *
-print(Decimal(num_1))
-
